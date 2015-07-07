2217f9b10ea5a29957356b2d9ed311b6e793d62e
Hackbright Student <info@hackbrightacademy.com>
2015-07-07 16:40:38 -0700

    Working make_chains function

--- markov.py
+++ markov.py
@@ -5,26 +5,39 @@ opened_file = open('green-eggs.txt')
 def make_chains(corpus):
     """Takes input text as string; returns dictionary of markov chains."""
 
-    first_word = 0
-    second_word = 1
-    third_word = 2
+    # first_word = 0
+    # second_word = 1
+    # third_word = 2
 
     list_of_words = corpus.read().replace("\n", " ").split()
 
     dict_tuples = {}
 
-    for word in list_of_words:
-        tuple_pair = (list_of_words[first_word], list_of_words[second_word])
-        if list_of_words[third_word] == list_of_words[-1]:
-            print dict_tuples
-            return
-        else: 
-            dictionary = dict_tuples[tuple_pair] = list_of_words[third_word] #.GET!! CHANGE IT
-            first_word += 1
-            second_word += 1
-            third_word += 1
-    
+    for i in range(len(list_of_words)-2):
+        key = (list_of_words[i], list_of_words[i+1])
+        value = list_of_words[i+2]
 
+        if key not in dict_tuples:
+            dict_tuples[key] = []
+
+        dict_tuples[key].append(value)
+
+        # if list_of_words[third_word] == list_of_words[-1]:
+        #     print dict_tuples
+        #     return
+        # else: 
+        #     value = dict_tuples.get([key], value.append(list_of_words[third_word]))
+        #     first_word += 1
+        #     second_word += 1
+        #     third_word += 1
+
+        # if tuple_pair not in dict_tuples:
+        #     list_of_following_words = []
+        #     dict_tuples[tuple_pair] = list_of_following_words.append(third_word)
+        # if tuple_pair in dict_tuples:
+        #     dict_tuples[tuple_pair] = # ad
+
+    print dict_tuples
 
 make_chains(opened_file)
 
