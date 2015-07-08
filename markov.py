96d44b6f2ce753bca74fd3c0e6a6cacb5c875fd1
Hackbright Student <info@hackbrightacademy.com>
2015-07-07 18:00:55 -0700

    have working dictionary but need to write make_text function

--- markov.py
+++ markov.py
@@ -1,15 +1,15 @@
 import sys
+import random
 
 opened_file = open('green-eggs.txt')
+# print type(opened_file)
 
 def make_chains(corpus):
     """Takes input text as string; returns dictionary of markov chains."""
 
-    # first_word = 0
-    # second_word = 1
-    # third_word = 2
-
     list_of_words = corpus.read().replace("\n", " ").split()
+    # print type(corpus)
+    # print type(list_of_words)
 
     dict_tuples = {}
 
@@ -37,19 +37,27 @@ def make_chains(corpus):
         # if tuple_pair in dict_tuples:
         #     dict_tuples[tuple_pair] = # ad
 
-    print dict_tuples
+    return dict_tuples
 
 make_chains(opened_file)
 
-# def make_text(chains):
-#     """Takes dictionary of markov chains; returns random text."""
+# dictionary = make_chains(opened_file)
+# print dictionary    
 
-#     return "Here's some random text."
+def make_text(dict_tuples):
+    """Takes dictionary of markov chains; returns random text."""
+    # make_chains(opened_file) 
+    # http://stackoverflow.com/questions/16043797/python-passing-variables-between-functions
+    print dict_tuples
+    # random.sample(make_chains(opened_file),1)
 
+    # return "Here's some random text."
 
-# # Change this to read input_text from a file, deciding which file should
-# # be used by examining the `sys.argv` arguments (if neccessary, see the
-# # Python docs for sys.argv)
+make_text(dict_tuples)
+
+# Change this to read input_text from a file, deciding which file should
+# be used by examining the `sys.argv` arguments (if neccessary, see the
+# Python docs for sys.argv)
 
 # input_text = "Some text"
 
