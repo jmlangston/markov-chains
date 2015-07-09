7cabd9050229d6bcad5e5dc753d74b65ec85accd
Hackbright Student <info@hackbrightacademy.com>
2015-07-09 13:01:16 -0700

    finished make_text func

--- markov.py
+++ markov.py
@@ -2,14 +2,11 @@ import sys
 import random
 
 opened_file = open('green-eggs.txt')
-# print type(opened_file)
 
 def make_chains(corpus):
     """Takes input text as string; returns dictionary of markov chains."""
 
     list_of_words = corpus.read().replace("\n", " ").split()
-    # print type(corpus)
-    # print type(list_of_words)
 
     dict_tuples = {}
 
@@ -22,46 +19,27 @@ def make_chains(corpus):
 
         dict_tuples[key].append(value)
 
-        # if list_of_words[third_word] == list_of_words[-1]:
-        #     print dict_tuples
-        #     return
-        # else: 
-        #     value = dict_tuples.get([key], value.append(list_of_words[third_word]))
-        #     first_word += 1
-        #     second_word += 1
-        #     third_word += 1
-
-        # if tuple_pair not in dict_tuples:
-        #     list_of_following_words = []
-        #     dict_tuples[tuple_pair] = list_of_following_words.append(third_word)
-        # if tuple_pair in dict_tuples:
-        #     dict_tuples[tuple_pair] = # ad
-
     return dict_tuples
 
 dictionary = make_chains(opened_file)
 
-# dictionary = make_chains(opened_file)
-# print dictionary    
 
-def make_text(dictionaryv2):
+def make_text(markov_dictionary):
     """Takes dictionary of markov chains; returns random text."""
-    our_string = ""
-    for index in dictionaryv2: 
-        random_key = random.choice(dictionaryv2.keys())
-        print random_key
-        value2 = dictionaryv2[random_key]
-        random_value = random.choice(value2)
-        print random_value
-        new_key = (random_key[1], random_value)
-        print new_key
-        new_value = random.choice(new_key)
-        our_string = our_string + random_key[0] + random_key[1] + new_value
 
-    if new_key not in dictionaryv2:
-        False
+    key = random.choice(markov_dictionary.keys())
+    value = []
 
-    # return "Here's some random text."
+    while key in markov_dictionary:
+        value2 = random.choice(markov_dictionary[key])
+        value.append(value2)
+        key = (key[1], value2)
+        print key
+        # if value > 100:
+        #     break
+
+    print " ".join(value)
+     
 
 make_text(dictionary)
 
@@ -69,12 +47,12 @@ make_text(dictionary)
 # be used by examining the `sys.argv` arguments (if neccessary, see the
 # Python docs for sys.argv)
 
-# input_text = "Some text"
+input_text = "Some text"
 
-# # Get a Markov chain
-# chain_dict = make_chains(input_text)
+# Get a Markov chain
+chain_dict = make_chains(input_text)
 
-# # Produce random text
-# random_text = make_text(chain_dict)
+# Produce random text
+random_text = make_text(chain_dict)
 
-# print random_text
+print random_texts
